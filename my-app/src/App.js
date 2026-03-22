fd49d7d887fd28eb83aa58af1212c6425f7a099c
HBica05 <bicahaadiyah@gmail.com>
2026-03-22 20:04:53 +0200

    feat: add protected routes for Skills, Exchanges and Profile

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Navbar from './components/Navbar';
+import ProtectedRoute from './components/ProtectedRoute';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Contact from './pages/Contact';
+import Skills from './pages/Skills';
+import SkillForm from './pages/SkillForm';
+import Exchanges from './pages/Exchanges';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -17,6 +22,20 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/skills/new" element={
+            <ProtectedRoute><SkillForm /></ProtectedRoute>
+          } />
+          <Route path="/skills/:id/edit" element={
+            <ProtectedRoute><SkillForm /></ProtectedRoute>
+          } />
+          <Route path="/exchanges" element={
+            <ProtectedRoute><Exchanges /></ProtectedRoute>
+          } />
+          <Route path="/profile" element={
+            <ProtectedRoute><Profile /></ProtectedRoute>
+          } />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
     </AuthProvider>
